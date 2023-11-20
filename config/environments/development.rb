1192402eb683d48b09a3e6744a7e42ab0eb9e2a1
FarouqJalabi <farouqjalabi@gmail.com>
2023-11-20 23:58:08 +0100

    Added tailwind

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,9 @@ Rails.application.configure do
   # * So url will go to localhost
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # * For tailwind
+  config.assets.debug = true
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.

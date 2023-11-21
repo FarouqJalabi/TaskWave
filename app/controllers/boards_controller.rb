ae9fbec4a9dbc944369dcd3ffe77eb079c86da5b
FarouqJalabi <farouqjalabi@gmail.com>
2023-11-21 21:27:47 +0100

    Added lists to boards

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,14 @@
 class BoardsController < ApplicationController
 
+  before_action :set_board, :show
   def index
     @boards = Board.all
+  end
+  def show
+  end
+
+  private
+  def set_board
+    @board = Board.find(params[:id])
   end
 end
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,5 @@
 class Board < ApplicationRecord
   # belongs_to :creator, :class_name => 'User', :foreign_key => 'creator_id'
   belongs_to :creator, :class_name => 'User', dependent: :destroy
+  has_many :lists, dependent: :destroy
 end

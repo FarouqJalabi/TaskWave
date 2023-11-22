f830b94406c2fcfd7d21da54c4765856151c023d
red cube <Farouqjalabi@gmail.com>
2023-11-22 21:08:36 +0100

    Tasks with due dates indecators

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,12 @@
 class Task < ApplicationRecord
   belongs_to :list
+  def due_color
+    if due_date < 3.day.from_now
+      'urgent'
+    elsif due_date < 7.day.from_now
+      'approaching'
+      else
+        'comfortable'
+    end
+  end
 end

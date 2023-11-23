e602b8555c95fad3ccd5060cf7d6aefa40a84bf8
red cube <Farouqjalabi@gmail.com>
2023-11-23 18:45:46 +0100

    Ability to add lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,7 @@
 class ListsController < ApplicationController
+  def create
+    @board = Board.find(params[:board_id])
+    @list = @board.lists.create(name: "New list")
+    redirect_to board_path(@board)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :boards
+  resources :boards do
+    resources :lists do
+      resources :tasks
+    end
+  end
+
   # Defines the root path route ("/")
   root "boards#index"
 end

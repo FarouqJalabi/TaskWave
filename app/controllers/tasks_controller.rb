c7166f0cdcc67184d42c6aa141d9adb875e9beec
red cube <Farouqjalabi@gmail.com>
2023-11-24 20:25:34 +0100

    Saving drag of tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,27 @@
 class TasksController < ApplicationController
   def create
+    # ? Why find board if we have list_id
     @board = Board.find(params[:board_id])
     @list = @board.lists.find(params[:list_id])
     @task = @list.tasks.create(name: "New task")
     redirect_to board_path(@board)
+  end
+
+  def update
+    puts "Yes sir 1234"
+    @task = Task.find(params[:id])
+    @task.update task_params
+    @task.save
+    puts "Yes sir 1234", @task
+    # For our stimulus
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :list_id)
   end
 end
--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -1,31 +1,57 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = [ "draggable" ]
+  static targets = ["draggable"];
+
   connect() {
-    this.element.addEventListener("drop", this.drop)
-    this.element.addEventListener("dragover", this.allowDrop)
+    this.element.addEventListener("drop", this.drop);
+    this.element.addEventListener("dragover", this.allowDrop);
   }
-  draggableTargetConnected(element){
-    element.draggable = true
-    element.addEventListener("dragstart", this.drag)
-    element.addEventListener("drop", this.drop)
+
+  draggableTargetConnected(element) {
+    element.draggable = true;
+    element.addEventListener("dragstart", this.drag);
+    element.addEventListener("drop", this.drop);
   }
+
   drag(e) {
     e.dataTransfer.setData("text", e.target.id);
+    console.log(e.target.id);
   }
 
   allowDrop(e) {
     e.preventDefault();
   }
+
   drop(e) {
     e.preventDefault();
-    let data = e.dataTransfer.getData("text");
-    if (e.target.draggable) {
-      e.target.parentElement.appendChild(document.getElementById(data));
-      e.target.style = "";
-    } else {
-      e.target.appendChild(document.getElementById(data));
-    }
+    const data = e.dataTransfer.getData("text");
+    const taskElement = document.getElementById(data);
+    const listElement = e.target.closest(".list");
+
+    listElement.appendChild(taskElement);
+    const updatePath = taskElement.dataset.updatePath;
+    console.log(updatePath);
+    let formData = new FormData();
+    formData.append("task[list_id]", listElement.id.split("-")[1]);
+    fetch(updatePath, {
+      body: formData,
+      method: "PATCH",
+      dataType: "script",
+      credentials: "include",
+
+      headers: {
+        "X-Requested-With": "XMLHttpRequest",
+        "X-CSRF-Token": document.head.querySelector("meta[name=csrf-token]")
+          ?.content,
+      },
+    }).then(function (response) {
+      if (response.status !== 204) {
+        console.log("Error:");
+        console.log(response);
+      }
+    });
   }
+
+  // updateRails(taskElement, listId) {}
 }

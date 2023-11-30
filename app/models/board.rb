9939bce83191d607db17d0f33ca496b45833ce37
FarouqJalabi <farouqjalabi@gmail.com>
2023-11-30 21:21:41 +0100

    Using image url in _board

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,13 +2,13 @@ class Board < ApplicationRecord
   # Validations
   validates_presence_of :name
   validates_presence_of :public
-
+  validates_presence_of :background_url
   # https://github.com/rails/rails/pull/49769#issuecomment-1777833124
   attribute :background_url
+  # key is url, value is alt text
   enum background_url: {
     "https://images.unsplash.com/photo-1486299267070-83823f5448dd?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":"An image of big ben",
   }
-
 
   # Relations
   belongs_to :creator, :class_name => 'User', dependent: :destroy

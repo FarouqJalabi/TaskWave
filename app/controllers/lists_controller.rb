49810c432107510c823ef653bebaa6c22882c4a3
FarouqJalabi <farouqjalabi@gmail.com>
2023-12-04 20:32:00 +0100

    Remove lists (when editing)

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,9 +10,15 @@ class ListsController < ApplicationController
     @list.update(list_params)
     redirect_to board_path(@list.board_id)
   end
+
+  def destroy
+    @list = List.find(params[:id])
+    board_id = @list.board_id
+    @list.destroy
+    redirect_to board_path(board_id), status: :see_other
+  end
   private
   def list_params
     params.require(:list).permit(:name)
-
   end
 end
